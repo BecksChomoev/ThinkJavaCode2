c5c37f008b9ebe8c099a0daeff737e6297fbfbf2
BecksChomoev <chomoevbeksultan@gmail.com>
2026-03-09 20:34:13 -0400

    Ex 4.1 is done.

---
+++ ch04/DateFromSecondChapter.java
@@ -0,0 +1,19 @@
+public class DateFromSecondChapter {
+    public static void printAmerican(String day, int date, String month, int year) {
+        System.out.println("American format: " + day + ", " + month + " " + date + ", " + year);
+    }
+
+    public static void printEuropean(String day, int date, String month, int year) {
+        System.out.println("European format: " + day + " " + date + ", " + month + " " + year);
+    }
+
+    public static void main(String [] args) {
+        String day = "Monday";
+        int date = 9;
+        String month = "March";
+        int year = 2026;
+
+        printAmerican(day, date, month, year);
+        printEuropean(day, date, month, year);
+    }
+}
--- ch04/PrintTwice.java
+++ ch04/PrintTwice.java
@@ -1,3 +1,4 @@
+
 public class PrintTwice {
 
     public static void printTwice(String s) {
