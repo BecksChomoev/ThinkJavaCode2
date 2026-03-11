2cbf4546fc9c9dec1be0293216a92725a4322bd2
BecksChomoev <chomoevbeksultan@gmail.com>
2026-03-11 14:17:59 -0400

    Ex 5.2 is done

---
+++ ch05/Ex_5_2.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex_5_2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+        int userGuess;
+        int userAttempts = 3;
+
+        System.out.println("I'm thinking of a number between 1 and 100");
+        System.out.println("(including both). Can you guess what it is?");
+
+        while(userAttempts != 0 ) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Type a number ");
+            userGuess = scanner.nextInt();
+
+            if (userGuess < number) {
+                System.out.println("Your number is less than the number I'm thinking about");
+                userAttempts--;
+            } else if (userGuess > number) {
+                System.out.println("Your number is bigger than the number I'm thinking about");
+                userAttempts--;
+            } else {
+                System.out.println("Congratulations! You guessed right!");
+            }
+
+            if (userAttempts == 0) {
+                System.out.println("I'm sorry you are out of attempts");
+                System.out.println("The number I was thinking of is: " + number);
+            }
+        }
+    }
+}
