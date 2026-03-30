497575a7107b0119e0fbb29002dfccd367e59f6d
BecksChomoev <chomoevbeksultan@gmail.com>
2026-03-30 17:07:34 -0400

    Ex with the Fruits is done.

--- ch07/Fruit.java
+++ ch07/Fruit.java
@@ -3,9 +3,10 @@
  */
 public class Fruit {
 
+    // Multiplication of all elements
     public static int banana(int[] a) {
-        int kiwi = 1;
-        int i = 0;
+        int kiwi = 1; // counter
+        int i = 0; // beginner
         while (i < a.length) {
             kiwi = kiwi * a[i];
             i++;
@@ -13,6 +14,7 @@ public class Fruit {
         return kiwi;
     }
 
+    // Looking for the number in the array;
     public static int grapefruit(int[] a, int grape) {
         for (int i = 0; i < a.length; i++) {
             if (a[i] == grape) {
@@ -22,6 +24,7 @@ public class Fruit {
         return -1;
     }
 
+    // Counting how many number appears in the array that we are looking for;
     public static int pineapple(int[] a, int apple) {
         int pear = 0;
         for (int pine: a) {
@@ -31,5 +34,4 @@ public class Fruit {
         }
         return pear;
     }
-
 }
